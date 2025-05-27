2a81180951bffd2adf4e9a38ec4d3c0a5b42d785
lotannauba <lotannauba23@gmail.com>
2025-05-27 15:11:46 +0100

    Add files via upload
    phase gate

---
+++ MainMenu.py
@@ -0,0 +1,37 @@
+## ""Using if statements to assign strings""
+## ""Using test development drive""
+## ""comparing a lot of chareacters to put each other in the string values""
+
+	List_menu["Add a task", "view all tasks", "mark as complete"]
+	
+	list_display["Display with their status"]
+
+
+
+list_menu = (1." Add a task ", 2."view users Tasks", 3."Mark task as complete", 4."Delete a task", 5. "Exit")
+
+	def_menu(userchoice == 1)
+		print(" 1. Add task") 
+		elif("please try again!")
+
+	def_menu(userchoice == 2)
+		print(" 2. view task")
+		elif("please try again!")
+
+	def_menu(userchoice == 3)
+		print(" 3. Mark task as complete")
+		elif("please try again!")
+
+	def_menu(Userchoice == 4)
+		print(" 4. Delete a task")
+		elif("Please try again!")
+
+	def_menu(Userchoice == 5)
+		print(" 5. Exit")
+		elif("Please Try again!")
+
+
+
+
+
+
---
+++ Testcase.py
@@ -0,0 +1,21 @@
+import to_do_list 
+
+import unnitest
+
+class Testbank(unnitest.Testcase))
+
+
+	@test
+
+def_test = 
+
+To_do_list = [buy groceries] 
+
+actual = []
+
+actual = to_do_list(Unnitest.Testcase)
+
+self.Assert Equals(actual,expected);
+
+
+ 
