d5f713a154d433152eeffb8b98587c1be3305b1f
lotannauba <lotannauba23@gmail.com>
2025-06-03 20:00:40 +0100

    Add files via upload

---
+++ BankApp.py
@@ -0,0 +1,77 @@
+bank charges = 100 
+withdrawal = 0
+AccountBalance = 0 
+AccountMultiples = 500,1000
+
+option = True
+while option:
+	message = '''
+welcome to my semicolon bank Atm app...
+
+what would you like to do today?
+
+1. Get balance  
+2. Your current balance 
+3. Customer withdrawal balance
+4. Customer withdrawal amount
+'''
+
+	print(withdrawal)
+		user_input = input("Enter your balance at the start: ")
+		match(user_input):
+
+	case "1":			
+	if (withdrawal < 500):
+	 print("You have to a enter a valid amount") 
+
+	Welcome to get balance:
+	Add your Account balance
+'''
+print(Your current balance)
+	match(user_input):
+		
+
+		case "2":
+			message = ""
+        	if(balance < withdrawal):
+	print(invalid balance)
+	
+	Your current balance:
+        Add your Account balance
+'''
+			
+	print(Customers withdrawal balance)
+	match(user_input):
+		
+		case "3":
+			message = ""
+		if(balance < withdrawal)
+	print():
+
+	Welcome to get balance:
+        Add your Account balance
+'''
+			
+
+	print(Customers withdrawal amount)
+	match(user_input):
+		
+		case "4":
+			message = ""
+		if()
+        Welcome to get balance:
+		
+        Add your Account balance
+'''
+		
+	
+			
+
+
+
+
+
+
+
+
+
